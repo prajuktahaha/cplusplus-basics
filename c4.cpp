a608e3333db8499d6bcff18654889ee7ac26cb6c
Prajukta <prajuktaha@gmail.com>
2025-11-15 07:54:40 +0530

    forloop basic

---
+++ c4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=1 ; i<=4 ; i++){
+        cout<<i<<endl;
+    } //this will print 1 , 2 , 3 ,4
+  for(i=1 ; i<=4 ; i--){
+    cout<<i<<endl;
+  } //ohgod a infinite number
+}
