1d05e53c9d2078112a87e4f6c40e13f372e3a503
Prajukta <prajuktaha@gmail.com>
2025-11-15 06:24:46 +0530

    sum and avg of 2 numbers

---
+++ c2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a, b , sum , avg;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    sum = a+b;
+    avg = sum/2;
+    cout<<"Sum is:"<<sum<<endl;
+    cout<<"Average is:"<<avg<<endl;
+    return 0;
+}
