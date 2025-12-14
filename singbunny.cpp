71cba0df2b276ce151c8ec48e0df8181d0c7faeb
Prajukta <prajuktaha@gmail.com>
2025-12-14 14:41:28 +0530

    singing bunny message

---
+++ singbunny.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+  std::cout<<"----------------------------\n";
+  std::cout<<"|                          |\n";
+  std::cout<<"|                          |\n";
+  std::cout<<"|     HAVE A GREAT         |\n";
+  std::cout<<"|       DAY AHEAD          |\n";
+  std::cout<<"|                          |\n";
+  std::cout<<"|                          |\n";
+  std::cout<<"|                          |\n";
+  std::cout<<"----------------------------\n";
+  std::cout<<"               ||           \n";
+  std::cout<<"( 0 o 0)       ||           \n";
+  std::cout<<"(      )       ||           \n";
+}
