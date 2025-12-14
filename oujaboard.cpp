bc9993c2a293d828cbce790ce9ebd65d9fa9155c
Prajukta <prajuktaha@gmail.com>
2025-12-14 14:29:57 +0530

    layout of ouja board

---
+++ oujaboard.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+  std::cout<<"------------------------------\n";
+  std::cout<<"|    YES             NO       |\n";
+  std::cout<<"|                             |\n";
+  std::cout<<"|      ABCDEFGHJIKLM          |\n";
+  std::cout<<"|      NOPQRSTUVWXYZ          |\n";
+  std::cout<<"|                             |\n";
+  std::cout<<"|        1234567890           |\n";
+  std::cout<<"------------------------------\n";
+}
