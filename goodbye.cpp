fc26c61de915194e4d1ad4b50a4ecb012aa6bb03
Prajukta <prajuktaha@gmail.com>
2025-12-14 14:08:24 +0530

    different ways to say goodbye

---
+++ goodbye.cpp
@@ -0,0 +1,8 @@
+//Create a C++ program that prints out a few different ways to say goodbye in different languages or slang.
+#include<iostream>
+
+int main(){
+  std::cout << "see yah\n";
+  std::cout << "good bye\n";
+  std::cout << "love yah\n";
+}
