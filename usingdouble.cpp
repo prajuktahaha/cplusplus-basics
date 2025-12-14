725bf673ec5cdfbc2cc38e2638f0bc1724835615
Prajukta <prajuktaha@gmail.com>
2025-12-14 16:18:37 +0530

    enter value in double
    Removed duplicate declaration comment for the reddit variable.

---
+++ usingdouble.cpp
@@ -0,0 +1,16 @@
+/*Declare a double variable called reddit and give it a value of its share of the last six months.
+
+Output the final number *\
+
+#include <iostream>
+
+int main() {
+  double reddit = 0;
+   reddit = 30.8;
+   reddit = 60.00;
+   reddit = 12.00;
+   reddit = 3.40;
+   reddit = 70.0;
+   reddit = 1.000;
+   std :: cout << reddit;
+}
