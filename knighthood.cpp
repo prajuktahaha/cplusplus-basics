a3b5797dd6a364fd8019733fa32b3c1629a911e9
Prajukta <prajuktaha@gmail.com>
2025-12-14 20:48:19 +0530

    Declare a variable called name with your name inside:

---
+++ knighthood.cpp
@@ -0,0 +1,10 @@
+//Declare a variable called name with your name inside:
+#include <iostream>
+
+int main() {
+  std :: string name = "Prajukta";
+  std::cout<< "in the name of the Warrior , i charge you to be the brave\n";
+  std::cout<< "In the name of the Father, I charge you to be just\n";
+  std::cout<<"In the name of the Mother, I charge you to defend the innocent.\n";
+  std::cout << "Arise " + name + "a knight of the Seven Kingdoms.\n";
+}
