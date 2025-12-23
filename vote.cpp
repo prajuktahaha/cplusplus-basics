a94fe87ce2887eb2ae29e1523d8e08c1abd08836
Prajukta <prajuktaha@gmail.com>
2025-12-23 23:59:42 +0530

    vote

---
+++ vote.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  int age;
+  bool citizen , registered;
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+  if(age >= 18){
+    std::cout << "You are eligible to vote";
+  }
+  else if (age < 18){
+    std::cout << "You are not old enough to vote";
+  }
+  else if (!citizen){
+    std::cout << "You are not eligible to vote";
+  }
+  else if (!registered){
+    std :: "you need to register";
+  }
+}
