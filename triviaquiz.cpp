3a4a1204cc322b91cd97dd428594634d17aef197
Prajukta <prajuktaha@gmail.com>
2025-12-26 19:13:40 +0530

    trivia quiz

--- triviaquiz.cpp
+++ triviaquiz.cpp
@@ -2,17 +2,17 @@
 
 int main() {
   int option;
-  std::cout<< Q1 ."What is the name of the world's longest river?\n";
-  std :: cout << 1 << "Missouri River\n";
-  std :: cout << 2 << "Nile\n";
-  std :: cout << 3 << "Amazon River\n";
-  std :: cout << 4 << "Yangtze River\n";
+  std::cout<<"Q1 . What is the name of the world's longest river?\n";
+  std :: cout << 1 << " . Missouri River\n";
+  std :: cout << 2 << " . Nile\n";
+  std :: cout << 3 << " . Amazon River\n";
+  std :: cout << 4 << " . Yangtze River\n";
   std :: cin >> option;
   if (option == 1){
     std::cout << "wrong answer";
   }
   else if (option == 2){
-    std::cout << "correct answer";
+    std::cout << "correct answer\n";
   }
   else if (option == 3){
     std::cout << "wrong nswer";
@@ -20,12 +20,11 @@ int main() {
   else if (option == 4){
     std::cout << "wrong answer";
   }
-  std :: cout << "NEXT QUESTION ;
-  std :: cout << Q2 . "How many bones are in the human body?\n";
-  std :: cout << 1 << "92\n";
-  std :: cout << 2 << "150\n";
-  std :: cout << 3 << "206\n";
-  std :: cout << 4 << "10\n";
+  std :: cout << " Q2 .How many bones are in the human body?\n";
+  std :: cout << 1 << " . 92\n";
+  std :: cout << 2 << " . 150\n";
+  std :: cout << 3 << " . 206\n";
+  std :: cout << 4 << " . 10\n";
   std :: cin >> option;
   if (option == 1){
     std::cout << "wrong answer";
@@ -39,4 +38,59 @@ int main() {
   else if (option == 4){
     std::cout << "wrong answer";
   }
+  std :: cout << " Q3 . What food never spoils? 😷\n";
+  std :: cout << 1 << " . salt\n";
+  std :: cout << 2 << " . chips\n";
+  std :: cout << 3 << " . icecream\n";
+  std :: cout << 4 << " . fruits\n";
+  std :: cin >> option;
+  if (option == 1){
+    std::cout << "correct answer\n";
+  }
+  else if (option == 2){
+    std::cout << "wrong answer";
+  }
+  else if (option == 3){
+    std::cout << "wrong answer\n";
+  }
+  else if (option == 4){
+    std::cout << "wrong answer";
+  }
+  std :: cout <<" Q4 .  What is the name of the tallest grass on earth?\n";
+  std :: cout << 1 << " . bamboo\n";
+  std :: cout << 2 << " . dhaiya\n";
+  std :: cout << 3 << " . rose\n";
+  std :: cout << 4 << " . pineapple\n";
+  std :: cin >> option;
+  if (option == 1){
+    std::cout << "correct answer\n";
+  }
+  else if (option == 2){
+    std::cout << "wrong answer";
+  }
+  else if (option == 3){
+    std::cout << "wrong answer\n";
+  }
+  else if (option == 4){
+    std::cout << "wrong answer";
+  }
+  std :: cout << " Q5 . How many dots are on a pair of dice?\n";
+  std :: cout << 1 << " . 12\n";
+  std :: cout << 2 << " . 21\n";
+  std :: cout << 3 << " . 42\n";
+  std :: cout << 4 << " . 36\n";
+  std :: cin >> option;
+  if (option == 1){
+    std::cout << "wrong answer";
+  }
+  else if (option == 2){
+    std::cout << "wrong answer";
+  }
+  else if (option == 3){
+    std::cout << "correct answer\n";
+  }
+  else if (option == 4){
+    std::cout << "wrong answer";
+  }
+  return 0;
 }
