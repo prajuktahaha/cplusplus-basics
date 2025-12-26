915095a004fd446f915f93081e888ff2eef088e0
Prajukta <prajuktaha@gmail.com>
2025-12-26 19:01:38 +0530

    quiz
    Added a second question about the number of bones in the human body.

--- triviaquiz.cpp
+++ triviaquiz.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   int option;
-  std::cout<<"What is the name of the world's longest river?\n";
+  std::cout<< Q1 ."What is the name of the world's longest river?\n";
   std :: cout << 1 << "Missouri River\n";
   std :: cout << 2 << "Nile\n";
   std :: cout << 3 << "Amazon River\n";
@@ -20,5 +20,23 @@ int main() {
   else if (option == 4){
     std::cout << "wrong answer";
   }
-  return 0;
+  std :: cout << "NEXT QUESTION ;
+  std :: cout << Q2 . "How many bones are in the human body?\n";
+  std :: cout << 1 << "92\n";
+  std :: cout << 2 << "150\n";
+  std :: cout << 3 << "206\n";
+  std :: cout << 4 << "10\n";
+  std :: cin >> option;
+  if (option == 1){
+    std::cout << "wrong answer";
+  }
+  else if (option == 2){
+    std::cout << "wrong answer";
+  }
+  else if (option == 3){
+    std::cout << "correct answer\n";
+  }
+  else if (option == 4){
+    std::cout << "wrong answer";
+  }
 }
