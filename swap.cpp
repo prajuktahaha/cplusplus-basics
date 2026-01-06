b2aa280dca1dd64225f316964403fb5ab9610e0f
Prajukta <prajuktaha@gmail.com>
2026-01-07 00:06:55 +0530

    swapping 2 nos
    Modified the swap program to accept user input for variables a and b instead of using fixed values.

--- swap.cpp
+++ swap.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a = 120 , b = 122;
-    cout << "Before swap a = " << a << "Before swapping b =" << b ;
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    int a , b;
+    cout << "Enter the value of a";
+    cin >> a;
+    cout << "Enter the value of b";
+    cin >> b;
+    cout << "Before swap a = " << a << "Before swapping b = " << b ;
+    a = a + b; //56
+    b = a - b; //23
+    a = a - b; //33
     cout << "\nAfter swap a = " << a << " After swapping b =" << b ;
     return 0;
 }
