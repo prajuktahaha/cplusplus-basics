25388b6f93ac904548c01a3d96dfffc02de52a61
Prajukta <prajuktaha@gmail.com>
2026-01-13 20:49:58 +0530

    area of the circle

---
+++ areacircle.cpp
@@ -0,0 +1,13 @@
+//area of cirle
+#include<iostream>
+using namespace std;
+int main()
+{
+   float radius , area , pi; 
+   pi = 3.14;
+   cout << "Enter radius of the cirle: ";
+   cin >> radius;
+   area = pi * radius * radius;
+   cout << "Area of the cicrle is " << area;
+   return 0;
+} 
