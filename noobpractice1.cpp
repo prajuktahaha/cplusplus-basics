e6d7de77b9ff8de53935ec03f7491a54c3c7540a
Prajukta <prajuktaha@gmail.com>
2026-01-13 16:20:37 +0530

    just a noob practice

---
+++ noobpractice1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Prajukta" << endl;
+    int x ;
+    cout << "Enter the value of x" << endl;
+    cin >> x;
+    cout << x + 10; //the mistake i did here was i added cin >> x + 10 ,  which is wrong
+    return 0;
+}
