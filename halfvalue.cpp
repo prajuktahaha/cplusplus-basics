abe93a6b4bd5130d1362a31285f6a13293c507c7
Prajukta <prajuktaha@gmail.com>
2026-01-13 21:56:35 +0530

    half value of x

---
+++ halfvalue.cpp
@@ -0,0 +1,11 @@
+//take x input and print its half value
+#include<iostream>
+using namespace std;
+int main()
+{
+   float x;
+   cout << "Enter the value of x : " << endl;
+   cin >> x;
+   float half = x/2;
+   cout << "the half value of x is : " << half << endl;
+}    
