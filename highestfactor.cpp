bc34179d02347369081d8bc5f9ad49fa0efab386
Prajukta <prajuktaha@gmail.com>
2026-01-14 07:09:28 +0530

    highest facorial

---
+++ highestfactor.cpp
@@ -0,0 +1,14 @@
+//highest factor
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n:";
+    cin >> n;
+    int f = 1;
+    for(int i = 1 ; i<n ; i++){
+        if(n%i==0) f=i;
+    }
+    cout << "Highest factor is: " << f;
+    return 0;
+}
